540b3b6ce8f1c585239735ee055e2f2febc6a0b7
Brad McKinney <brad.mckin@gmail.com>
2019-02-20 18:10:01 -1000

    edit User model, fix relations

--- database/models/Contact.js
+++ database/models/Contact.js
@@ -1,11 +1,10 @@
 const bookshelf = require('./bookshelf');
+const User = require('./User');
 
 class Contact extends bookshelf.Model {
   get tableName() { return 'contacts'; }
   get hasTimestamps() { return true; }
-  user() {
-    return this.belongsTo('User');
-  }
+  user() { return this.hasOne('User', 'id', 'created_by'); }
 }
 
 module.exports = bookshelf.model('Contact', Contact);
