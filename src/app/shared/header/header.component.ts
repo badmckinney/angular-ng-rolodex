bc19b85502395b069cf1cc4ba069774fec9c2352
Brad McKinney <brad.mckin@gmail.com>
2019-02-22 12:21:39 -1000

    integrate authentication functionality to header component

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { SessionService } from 'src/app/services/session.service';
 
 @Component({
   selector: 'app-header',
@@ -7,5 +10,25 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
-  constructor() { }
+  user: Object;
+
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private session: SessionService
+  ) {
+    this.user = this.session.getSession();
+  }
+
+  isLoggedIn() {
+    return this.session.isLoggedIn();
+  }
+
+  login() {
+    return this.router.navigate(['/login']);
+  }
+
+  logout() {
+    return this.auth.logout();
+  }
 }
